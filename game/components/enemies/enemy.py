fe630ede9fd8a30f1747c52d7194fa191a287b65
JoseSebastianValdiviaMiranda <josesebastianvaldiviamiranda@gmail.com>
2023-07-20 00:16:07 -0400

    Third-Class

--- game/components/enemies/enemy.py
+++ game/components/enemies/enemy.py
@@ -2,41 +2,35 @@ import random
 import pygame
 
 from pygame.sprite import Sprite
+from game.components.bullets.bullet import Bullet
 from game.utils.constants import ENEMY_1, ENEMY_2, SCREEN_WIDTH
 
 class Enemy(Sprite):
     ENEMY_WIDTH = 40
     ENEMY_HEIGHT = 60
-    Y_POS = 0
-    #X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]    
-    SPEED_Y = 3
-    SPEED_X = 5
-    SHIP = {0:'enemy_1', 1:'enemy_2'}
+    Y_POS = 20
+    X_POS_LIST = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550]    
+    SPEED_Y = 4
+    SPEED_X = 5    
     MOV_X = {0:'left', 1:'right'}
+    IMAGE = {1: ENEMY_1, 2: ENEMY_2}
 
-    def __init__(self):
-        #self.image = pygame.transform.scale(self.selected_enemy, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
-        self.type_of_ship()
+    def __init__(self, image = 1, speed_x = SPEED_X, speed_y = SPEED_Y, move_x_for = [30, 100]):
+        self.image = pygame.transform.scale(self.IMAGE[image], (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))        
         self.rect = self.image.get_rect()              
-        #self.rect.x = self.X_POS_LIST[random.randint(0, 10)]       
-        self.rect.x = random.randrange(50, SCREEN_WIDTH, 50)
-        self.rect.y = self.Y_POS - self.ENEMY_HEIGHT
-        self.speed_y = self.SPEED_Y
-        self.speed_x = self.SPEED_X
+        self.rect.x = self.X_POS_LIST[random.randint(0, 10)]        
+        self.rect.y = self.Y_POS
+        self.speed_y = speed_y
+        self.speed_x = speed_x
         self.movement_x = self.MOV_X[random.randint(0, 1)]
-        self.move_x_for = random.randint(30, 100)
+        self.move_x_for = random.randint(move_x_for[0], move_x_for[1])
         self.index = 0
-
-    def type_of_ship(self):
-        type_of_enemy = self.SHIP[random.randint(0,1)]
-        #print(type_of_enemy)   
-        if type_of_enemy == 'enemy_1':
-            self.image = pygame.transform.scale(ENEMY_1, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))
-        else:
-            self.image = pygame.transform.scale(ENEMY_2, (self.ENEMY_WIDTH, self.ENEMY_HEIGHT))  
-
-    def update(self):
+        self.type = "enemy"
+        self.shooting_time = random.randint(30, 50)
+        
+    def update(self, game):
         self.rect.y += self.speed_y
+        self.shoot(game.bullet_manager)
         if self.movement_x == 'left':
             self.rect.x -= self.speed_x            
         else:
@@ -54,3 +48,10 @@ class Enemy(Sprite):
             elif self.movement_x == 'left':
                 self.movement_x = 'right'
             self.index = 0
+
+    def shoot(self, bullet_manager):
+        current_time = pygame.time.get_ticks()
+        if self.shooting_time <= current_time:
+            bullet = Bullet(self)
+            bullet_manager.add_bullet(bullet)
+            self.shooting_time += random.randint(30, 50)
